57ce65ecff6a9504fb6d9e5adbdb00b1aba13fcf
Mikaeri Ohana <mi_ohana@hotmail.com>
2017-11-12 21:11:30 -0200

    Data Bindings

---
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  // styleUrls: ['./data-binding.component.css'],
+  styles: [
+    `
+      .highlight {
+        background-color: yellow;
+        font-weight: bold;
+      }
+    `
+  ],
+  encapsulation: ViewEncapsulation.None
+})
+export class DataBindingComponent implements OnInit {
+
+  url = 'http://loiane.com';
+  cursoAngular = true;
+  urlImagem = 'http://lorempixel.com/400/200/';
+
+  valorAtual = '';
+  valorSalvo = '';
+
+  isMouseOver = false;
+
+  getValor() {
+    return 1;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  botaoClicado() {
+    alert('Botão clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+    this.valorAtual = ((<HTMLInputElement>evento.target).value);
+  }
+
+  salvarValor(valor) {
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut() {
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

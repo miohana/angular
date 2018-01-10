9d9efe1c3e42163f0fdc72f08c3304940d4363cc
Mikaeri Ohana <mi_ohana@hotmail.com>
2018-01-09 22:03:53 -0200

    Ajuste de segurança do Handlebars

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { DataBindingComponent } from './data-binding/data-binding.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DataBindingComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
